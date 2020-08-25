f6c19395a483af6aa347797ae42153937b0ad241
M. Cullen McClellan <matthew-mcclellan@lambdastudents.com>
2020-08-24 20:34:42 -0400

    Task 1 Complete

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
+console.log("Task 1a:");
 
 let votingAge = 25;
 if (votingAge > 18) {
     console.log(true);
 }
 
-
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
 
 
-
-
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
-
-
+console.log(Number("1999"));
 
 
 
 //Task d: Write a function to multiply a*b 
 
+/**
+ * Multiplies two numbers
+ * @param {Number} a  First number
+ * @param {Number} b  Second Number
+ */
+function multiply(a, b) {
+    return a * b;
+}
 
-
+console.log(multiply(8, 2));
 
 
 /************************************************************** Task 2 **************************************************************/
@@ -50,7 +55,7 @@ if (votingAge > 18) {
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+
 
 
 
@@ -62,8 +67,8 @@ if (votingAge > 18) {
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+
+
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
@@ -74,7 +79,7 @@ if (votingAge > 18) {
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+
 
 
 
@@ -83,7 +88,7 @@ if (votingAge > 18) {
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
+
 
 
 
@@ -96,10 +101,10 @@ if (votingAge > 18) {
 //70s should be Cs 
 //60s should be D 
 //and anything below 60 should be F
-  
 
-  
-  
+
+
+
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
